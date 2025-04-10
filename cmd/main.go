7c71cdaea1d38556bfbc58cb45165dea890dcb53
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2025-04-10 09:48:32 +0100

    feat(cmd): adds `gv add` command to add ts typings

--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,22 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	// Root command
+	var rootCmd = &cobra.Command{Use: "gv"}
+
+	// Add the 'add' command to root command
+	rootCmd.AddCommand(addCommand)
+
+	// Execute the root command
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
