b3fdd3290acdcf79271488d9364e1221a21d5701
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2025-04-25 08:00:47 +0100

    fix(examples/react): removes mode check for project build

--- examples/react/main.go
+++ examples/react/main.go
@@ -1,25 +1,15 @@
 package main
 
 import (
-	"embed"
-	"log"
-
 	"github.com/labstack/echo/v4/middleware"
-	"github.com/samber/lo"
 	"github.com/struckchure/gv"
 )
 
-//go:embed dist
-var _ embed.FS
-
 func main() {
 	srv := gv.NewServer(gv.ServerConfig{
-		Host: "localhost",
-		Port: 3000,
-
-		EsBuildOptions:    EsbuildOptions,
-		WatchPath:         lo.ToPtr("./"),
-		WatchExcludePaths: &[]string{"dist"},
+		Host:           "localhost",
+		Port:           3000,
+		EsBuildOptions: EsbuildOptions,
 	})
 
 	srv.Server().Use(middleware.StaticWithConfig(middleware.StaticConfig{
@@ -27,5 +17,5 @@ func main() {
 		Root:  "dist",
 	}))
 
-	log.Fatal(srv.Start())
+	srv.Start()
 }
